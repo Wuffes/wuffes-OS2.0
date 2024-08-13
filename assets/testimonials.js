453ae505623e424e657ce75180311e41d6ed5226
Vadym Mazurenko <vadym@wuffes.com>
2024-08-13 18:50:45 +0300

    code review fixes

--- assets/testimonials.js
+++ assets/testimonials.js
@@ -1,28 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const blocks = document.querySelectorAll('.block_video_wrapper');
+  const sections = document.querySelectorAll('.testimonials_slider_block-video-wrapper');
 
-  blocks.forEach((block) => {
-    block.addEventListener('click', (event) => {
-      const media = event.target.closest('.video-section__media');
-      const slideCardBottom = block.querySelector('.js_slide_card_bottom');
+  sections.forEach((section) => {
+    const blocks = section.querySelectorAll('.block_video_wrapper');
 
-      if (media && slideCardBottom) {
-        slideCardBottom.classList.add('active-class');
-      }
+    blocks.forEach((block) => {
+      block.addEventListener('click', (event) => {
+        const media = event.target.closest('.video-section__media');
+        const slideCardBottom = block.querySelector('.js_slide_card_bottom');
+
+        if (media && slideCardBottom) {
+          slideCardBottom.classList.add('active-class');
+        }
+      });
     });
-  });
 
-  const newNextButton = document.querySelector('.next-button.data-slider-button');
-  const newPrevButton = document.querySelector('.prev-button.data-slider-button');
+    const newNextButton = section.querySelector('.next-button.data-slider-button');
+    const newPrevButton = section.querySelector('.prev-button.data-slider-button');
 
-  const oldNextButton = document.querySelector('.slider-button--next');
-  const oldPrevButton = document.querySelector('.slider-button--prev');
+    const oldNextButton = section.querySelector('.slider-button--next');
+    const oldPrevButton = section.querySelector('.slider-button--prev');
 
-  newNextButton.addEventListener('click', function () {
-    oldNextButton.click();
-  });
+    newNextButton?.addEventListener('click', function () {
+      oldNextButton?.click();
+    });
 
-  newPrevButton.addEventListener('click', function () {
-    oldPrevButton.click();
+    newPrevButton?.addEventListener('click', function () {
+      oldPrevButton?.click();
+    });
   });
 });
