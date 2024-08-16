188a3c855848f3ec22f3ed2dd7e337acc8f8d1da
Misha Wuffes <mikhail@wuffes.com>
2024-08-17 00:19:44 +0300

    picker offers switching logic added

--- assets/section-product-main.js
+++ assets/section-product-main.js
@@ -1,12 +1,12 @@
 // Synchronize active offers inputs
-const radioGroups = document.querySelectorAll('.picker-dogsize_offers-group');
+const offersGroups = document.querySelectorAll('.picker-dogsize_offers-group');
 
-radioGroups.forEach(group => {
+offersGroups.forEach(group => {
   group.addEventListener('change', event => {
     if (event.target.tagName === 'INPUT' && event.target.type === 'radio') {
       const selectedIndex = [...group.querySelectorAll('input[type="radio"]')].indexOf(event.target);
 
-      radioGroups.forEach(otherGroup => {
+      offersGroups.forEach(otherGroup => {
         if (otherGroup !== group) {
           const correspondingRadio = otherGroup.querySelectorAll('input[type="radio"]')[selectedIndex];
           correspondingRadio.checked = true;
@@ -19,12 +19,21 @@ radioGroups.forEach(group => {
 // Switch between offer groups
 function updateOfferGroups() {
   const offerGroups = document.querySelectorAll('.picker-dogsize_offers-group');
+
+  let purchaseType = 'sub';
+
+  if (purchaseTypeInput.checked) {
+    purchaseType = 'sub';
+  } else {
+    purchaseType = 'otp';
+  }
+
   offerGroups.forEach(group => group.style.display = 'none');
 
   const activeInput = document.querySelector('.picker-dogsize_input:checked');
 
   if (activeInput) {
-    const activeGroupId = activeInput.getAttribute('data-offers-group');
+    const activeGroupId = activeInput.getAttribute('data-offers-group') + '-' + purchaseType;
     const activeGroup = document.getElementById(activeGroupId);
     if (activeGroup) {
       activeGroup.style.display = 'block';
@@ -33,8 +42,12 @@ function updateOfferGroups() {
 }
 
 const dogSizeInputs = document.querySelectorAll('.picker-dogsize_input');
+const purchaseTypeInput = document.getElementById('js__picker-purchase_type');
 dogSizeInputs.forEach(input => {
   input.addEventListener('change', updateOfferGroups);
 });
 
+purchaseTypeInput.addEventListener('change', updateOfferGroups);
+
 document.addEventListener('DOMContentLoaded', updateOfferGroups);
+
